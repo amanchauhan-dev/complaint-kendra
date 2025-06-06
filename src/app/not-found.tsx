3e378237f201548f3643b8a5471207ee14544b28
amanchauhan-dev <amanchauhan0435@gmail.com>
2025-06-06 13:12:27 +0530

    fix: Add underline class to ProgressBarLink in Not Found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -11,7 +11,7 @@ export default function page() {
         <div className="text-center p-10">
             <h1 className="text-4xl font-bold">Page Not Found</h1>
             <p className="text-gray-600 mt-2">This page doesn&apos;t exist.</p>
-            <ProgressBarLink href={'/'}>
+            <ProgressBarLink href={'/'} className="underline">
                 <Button variant={'link'}>
                     Home
                 </Button>
